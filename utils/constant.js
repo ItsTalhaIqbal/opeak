00fb59f4e4924ec7da837e2de461d45998d97fda
TALHA IQBAL <134729186+ItsTalhaIqbal@users.noreply.github.com>
2025-04-15 19:48:18 +0500

    Update constant.js

--- utils/constant.js
+++ utils/constant.js
@@ -1,6 +1,6 @@
 const baseURL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:8000'
-    : 'https://opeak-backend.vercel.app';
+    : 'https://opeak-backend.vercel.ap';
 
 export { baseURL };
